103e2a56423126aea0b68f502c6a665581663c32
Porosnicu Andrei Alexandru <110765280+Website-Free@users.noreply.github.com>
2023-02-12 18:50:28 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,322 @@
+let MCQS = [{
+    question: "Sarcina ta este să demolezi o cameră cu sistem de încălzire. Observați o placă fibroasă albă sub instalație. Bănuiți că aveți de-a face cu azbest. Ce aveti de facut?",
+    choice1: " Rupeți o piesă și analizați-o în laborator.",
+    choice2: "Avertizați supraveghetorul și clientul.",
+    choice3: "Udați placa, așezați-o cu atenție în folia de plastic și scoateți-o.",
+    choice4: "Toate de mai Sus.",
+    answer: 1
+},
+            {
+            question: "Sarcina dvs. este să îndepărtați îmbinările de ciment de pe peretele exterior la o înălțime de 10 m. Munca va dura o săptămână întreagă. Nivelul de zgomot este de 75 dB (A). Ce măsuri trebuie să luați pentru a lucra într-un mediu sigur?",
+    choice1: "Purtarea protecției auditive.",
+    choice2: "Utilizarea unei scări cu înălțimea de min. 12 m.",
+    choice3: "Punerea hamului.",
+    choice4: "Utilizarea unui schele connforme.",
+    answer: 3
+},
+            {
+            question: "Sarcina lui Alexandru este demolarea unei structuri împreună cu colegii. Andrei este titularul unui permis de muncă. Permisul este valabil o zi",
+    choice1: "Andrei trebuie să verifice dacă colegii de muncă respectă condițiile permisului.",
+    choice2: "După finalizarea tuturor lucrărilor, colegii trebuie să semneze un permis de muncă.",
+    choice3: "Firma autorizarta trebuie să explice asociației Andrei ce implica munca lor.",
+    choice4: "Poate sa lucreze 7 zile",
+    answer: 0
+},
+            {
+            question: "De ce ar trebui întotdeauna derulați complet un cablu prelungitor?",
+    choice1: "Toate variantele de mai jos",
+    choice2: "Pentru a verifica dacă cablul nu este deteriorat.",
+    choice3: "Pentru a preveni câmpurile electromagnetice.",
+    choice4: "Pentru a preveni generarea de căldură nedorită.",
+    answer: 3
+},
+            {
+            question: "Tu trebuie să inspectezi marginea acoperișului clădirii de birouri. Clădirea are 5 etaje. Pe acoperișul plat nu există securitate la capete. Cum puteți efectua inspecția în siguranță?",
+    choice1: "De la schele montate pe fațadă",
+    choice2: "Pe oricare schela",
+    choice3: "De pe o scară sprijinită pe fațadă.",
+    choice4: "De la pasarela de pe acoperiș.",
+    answer: 0
+},
+            {
+            question: "Ce este steigerkaart (cardul de schele)?",
+    choice1: "Denumirea schelei",
+    choice2: "Are numărul certificatului muncitorului de schele.",
+    choice3: " Puteți citi datele inspecției și sistemul de schele din acesta.",
+    choice4: "Indică dacă schela este sigură de utilizat",
+    answer: 3
+},
+            {
+               question: "Care afirmație este corectă?",
+    choice1: "O analiză a riscului în ultimul moment ar trebui efectuată la sfârșitul sarcinii.",
+    choice2: "O analiză de risc în ultimul moment nu este necesară dacă este disponibilă o analiză de risc bazată pe sarcini.",
+    choice3: "Analiza riscurilor de ultim moment este controlul siguranței la locul de muncă.",
+    choice4: "O analiză de risc în ultimul moment trebuie efectuata de inginer.",
+    answer: 2
+},{
+               question: "Responsabilitatea principală pentru siguranța la locul de muncă revine:",
+    choice1: "Paznicul",
+    choice2: "Inspecția muncii",
+    choice3: "Angajatului uzinei",
+    choice4: "Angajatorul",
+    answer: 3
+},
+            {
+               question: "Ce determină nivelul de risc în timpul muncii?",
+    choice1: "De acasa.",
+    choice2: "De la supravegherea exercitată de client.",
+    choice3: "Din planul de sănătate și siguranță (plan V & G).",
+    choice4: "De la locul de muncă.",
+    answer: 3
+},
+            {
+               question: "Care sunt diferitele faze in controlul și rezolvarea accidentelor?",
+    choice1: "Reportarea unui incident, ridicarea barierelor, implementarea activităților, încheierea incidentului.",
+    choice2: "La începutul Raportării incidentului, informarea pompierilor, terminarea pompierilor.",
+    choice3: "La început: reportarea unui incident, clasificarea incidentului, implementarea acțiunilor.",
+    choice4: "Raportarea unui incident.",
+    answer: 2
+},
+{
+
+               question: "Dragos lucreaza în incinta fabricii. Dragos lucrează singur și locul de muncă este zgomotos. Crede că aude o alarmă de scurgere a gazului. Dar Dragos nu este sigur. Ce ar trebui să facă?",
+    choice1: " Sa continuie munca.",
+    choice2: "Mergeți direct la punctul de întâlnire.",
+    choice3: "Apelați serviciul și întrebați dacă alarma a fost declanșată.",
+    choice4: "Sunați-l pe șef și întrebați-l dacă poate continua să lucreze.",
+    answer: 1
+},
+            {
+            question: "Care este riscul de benzen?",
+    choice1: "Toxic.",
+    choice2: "Benzenul este o substanță dăunătoare.",
+    choice3: "Benzenul este un plastic",
+    choice4: "Benzenul este un cancerigen.",
+    answer: 3
+},
+            {
+            question: "Ce ar trebui să fac când folosesc lanțul?",
+    choice1: "Nici un raspuns, nu este corect.",
+    choice2: "Să ungeți lanțul înainte de utilizare.",
+    choice3: "Strângeți în siguranță șuruburile bornei.",
+    choice4: "Lucrările trebuie efectuate de către doi oameni.",
+    answer: 2
+},
+            {
+            question: "Când este imposibil să lucrezi la înălțime?",
+    choice1: "Când forța vântului depășește 4 grade.",
+    choice2: "când forța vântului depășește 5 grade.",
+    choice3: "Cand forța vântului depășește 3 grade.",
+    choice4: "Când forța vântului depășește 6 grade.",
+    answer: 3
+},
+            {
+            question: "Care din urmatoarele este corecta:LMRA Analiza riscului in ultimul minut?",
+    choice1: "LMRA trebuie facuta la incheierea activitatii.",
+    choice2: "Toate variantele sunt gresite.",
+    choice3: "LMRA nu este necesara daca exista inventar de evaluare a riscurilor la locul de munca",
+    choice4: "LMRA consta in controlarea securitatii la locul de munca.",
+    answer: 3
+},
+            {
+            question: "Ce este important atunci când ridicaţi manual un obiect aflat pe jos?",
+    choice1: "Să puteţi întoarce lateral obiectul după ridicare.",
+    choice2: "Să ridicaţi obiectul cu spatele drept şi genunchii îndoiţi.",
+    choice3: "Sa indepartati cat mai mult picioarele.",
+    choice4: "Sa ridicati cu bratele drepte.",
+    answer: 1
+},
+            {
+               question: "Când trebuie să purtaţi ochelarii de protecţie?",
+    choice1: "La polizare, tăiere şi găurire.",
+    choice2: "Nici o varianta nu este corecta.",
+    choice3: "La sudurile autogene",
+    choice4: "La cuplarea şi decuplarea furtunurilor produsului.",
+    answer: 0
+},{
+               question: "Cum trebuie plasată o butelie de acetilenă în timpul sudării?",
+    choice1: "Butelia trebuie să stea într-un unghi de cel puţin 30° faţă de podea.",
+    choice2: "Butelia trebuie să stea numai în poziţie laterala.",
+    choice3: "Butelia trebuie să stea numai în poziţie verticală.",
+    choice4: "Butelia poate fi aşezată orizontal pe podea.",
+    answer: 0
+},
+            {
+               question: "Care este unul dintre riscurile utilizării unui motostivuitor?",
+    choice1: "Stricarea aparatului",
+    choice2: "Afectarea auzului celor din jur.",
+    choice3: "Accidentarea persoanelor.",
+    choice4: "Murdărirea sarcinii.",
+    answer: 2
+},
+            {
+               question: "Care este unghiul maxim al celor două lanțuri suspendate de care este atașată sarcina?",
+    choice1: "45°",
+    choice2: "90°",
+    choice3: "150°",
+    choice4: "120°",
+    answer: 3
+
+},
+{
+
+                question: "La ce înălțime ar trebui să se utilizeze dispozitive anti-cădere?",
+    choice1: "De la 1 metru.",
+    choice2: "De la 2 metri.",
+    choice3: "De la 3 metri.",
+    choice4: "Peste 2,5 metri.",
+    answer: 1
+},
+            {
+            question: "Temperatura de autoaprindere este …",
+    choice1: "cea mai scăzută temperatură la care lichidul se poate aprinde din lumină",
+    choice2: "cea mai mica temperatură la care substanța aprinde",
+    choice3: "cea mai mare temperatură la care substanța aprinde",
+    choice4: "cea mai scăzută temperatură la care o substanță se auto-aprinde fără o sursă de aprindere externă",
+    answer: 3
+},
+            {
+            question: "Aveți o etichetă veche a substanței periculoase. Unde sunt informațiile despre măsurile de securitate?",
+    choice1: "Pe baza descrierii de sub litera M.",
+    choice2: "Pe baza descrierii de sub litera R.",
+    choice3: "Pe baza descrierii de sub litera P.",
+    choice4: "Pe baza descrierii de sub litera S.",
+    answer: 3
+},
+            {
+            question: "Ce este analiza riscului de activitate (TRA)?",
+    choice1: "Analiza scrisă a riscurilor care pot apărea în timpul lucrului cu cel mai mic grad de risc.",
+    choice2: "O listă scrisă a lucrărilor care pot duce la amenințări în întreprindere.",
+    choice3: "Analiza scrisă a riscurilor care pot apărea în timpul lucrului cu cel mai înalt grad de risc.",
+    choice4: "Evaluarea scrisă a sarcinilor enumerate în documentul de recunoaștere și evaluare a pericolelor.",
+    answer: 2
+},
+            {
+            question: "Când, conform prevederilor Legii privind condițiile de muncă, ar trebui să încetez munca?",
+    choice1: "Daca colegu iti spune",
+    choice2: "În caz de condiții meteorologice nefavorabile",
+    choice3: "În caz de pericol",
+    choice4: "Dacă lucrarea durează mai mult de 3 ore",
+    answer: 2
+},
+            {
+            question: "Stingătorul cu spumă funcționează pe principiul:",
+    choice1: "răcind sursa de incendiu.",
+    choice2: "Congelare.",
+    choice3: "răcind și deplasând catalizatorul de oxigen.",
+    choice4: "întreruperea alimentării cu oxigen.",
+    answer: 3
+},
+            {
+               question: "Selectați o trăsătură care este caracteristică unui spațiu închis",
+    choice1: "Exista doar o intrare.",
+    choice2: "Există puțin oxigen în cameră",
+    choice3: "Nu există nici o ventilație naturală.",
+    choice4: "Are o singură intrare și o ieșire.",
+    answer: 2
+},{
+               question: "Pentru a lucra în condiții de siguranță în spații închise, trebuie să:",
+    choice1: "Efectuați lucrări de cel puțin două persoane.",
+    choice2: "Puteti lucra si singur.",
+    choice3: "Testați semnalele de alarmă.",
+    choice4: "Angajați 2 gardieni.",
+    answer: 0
+},
+            {
+               question: "Ce poate fi cauzat de zgomotul excesiv?",
+    choice1: "Dureri de cap.",
+    choice2: "Inflamația urechii medii.",
+    choice3: "Cefalee cronică.",
+    choice4: "Hipoacuzie permanentă.",
+    answer: 3
+},
+            {
+               question: "Cum se produce electricitatea statică?",
+    choice1: "Toate variantele.",
+    choice2: "Prin curgerea curentului electric prin electrodul de pământ.",
+    choice3: "Prin atingerea părților sub tensiune.",
+    choice4: "Prin frecare între aer, substanțe (lichide), materiale și obiecte.",
+    answer: 3
+},
+{
+               question: "Valoarea curentului de șoc electric depinde de:",
+    choice1: " rezistenta",
+    choice2: "intensitatea, tensiunea, rezistența temperaturii.",
+    choice3: "substratului.",
+    choice4: "durata atingerii",
+    answer: 1
+},
+            {
+            question: "Toate mașinile electrice trebuie verificate de un specialist:",
+    choice1: "o dată pe an.",
+    choice2: "După fiecare reparație.",
+    choice3: "La 2 ani.",
+    choice4: "La fiecare 5 ani.",
+    answer: 0
+},
+            {
+            question: "Cum este securizată o gaură minim periculoasă în perete sau o șanț adânc?",
+    choice1: "Pune un gard.",
+    choice2: "Puneți o Bandă de plastic.",
+    choice3: "Toate Variantele",
+    choice4: "Postați un semn de avertizare.",
+    answer: 0
+},
+            {
+            question: "Puteți citi capacitatea maximă de încărcare a centurii din:",
+    choice1: "Manual",
+    choice2: "Incărcătura fiind ridicată.",
+    choice3: "Ambalaj",
+    choice4: "Pe banda de autocolant.",
+    answer: 3
+},
+            {
+            question: "Cum stingem un incendiu de echipamente electrice?",
+    choice1: "Toate variantele de mai jos.",
+    choice2: "cu Apă",
+    choice3: "CO2 sau pulbere.",
+    choice4: "O pătură",
+    answer: 2
+},
+            {
+            question: "Funcționarea stingătorului cu CO2 se bazează pe :",
+    choice1: "Înlocuirea oxigenului.",
+    choice2: "Congelare",
+    choice3: "Inghetare",
+    choice4: "Acționând ca un catalizator negativ",
+    answer: 0
+},
+            {
+               question: "Trebuie să lucrezi într-o cameră închisă. Măsurătorile arată că concentrația de oxigen este de 18%. Ce trebuie să faci pentru a începe să lucrezi?",
+    choice1: "Ventilați camera în mod mecanic și măsurați din nou concentrația de oxigen.",
+    choice2: "Folosiți o mască de filtrare",
+    choice3: "Nimic, concentrația de oxigen este suficient de mare.",
+    choice4: "Folositi un prosop",
+    answer: 0
+},{
+               question: "Ce impune angajatorul, Legea privind condițiile de muncă?",
+    choice1: "Furnizați echipamente de protecție personală angajaților",
+    choice2: "Abordați doar pericolele care ar putea duce la un accident",
+    choice3: "Masina personala.",
+    choice4: "Asigurați o masă caldă la locul de muncă",
+    answer: 0
+},
+            {
+               question: "Scopul raportului de amenințare și al evaluării riscurilor este?",
+    choice1: "Angajatorul trebui sa se ocupe.",
+    choice2: "Ascunderea amenințărilor existente",
+    choice3: "Eliminarea tuturor amenințărilor",
+    choice4: "Controlul riscurilor",
+    answer: 3
+},
+            {
+               question: "La ce poate conduce prea mult zgomot?",
+    choice1: "Asurzire timpurie.",
+    choice2: "Perturbarea comunicarii si probleme cu stomacul si de digestie.",
+    choice3: "Ameteala si dureri ale urechilor",
+    choice4: "Pierderea vederi.",
+    answer: 1
+
+
+
+}];
